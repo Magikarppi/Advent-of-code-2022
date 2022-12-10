572745861bbbc257cb31ee0c7fc762c8f4c9904c
Sami H <samih90@gmail.com>
2022-12-10 16:09:57 +0200

    Add solution for day-5 part-2 + refactor + comments

--- day-5/supply-stacks.js
+++ day-5/supply-stacks.js
@@ -4,10 +4,12 @@ const organizeCratesByColumns = (crateSection, columnIndexes) => {
   const columns = columnIndexes.length;
 
   let cratesByColumns = {};
+  // create an object that has keys that correspond to column numbers and values which are arrays of crate ids like ["Z", "C"]
   for (let i = 1; i <= columns; i++) {
     cratesByColumns[i] = [];
   }
 
+  // parse data from text input to the crates-column object
   for (let i = 0; i < crateSection.length; i++) {
     for (let j = 0; j < columnIndexes.length; j++) {
       const currColumnValue = crateSection[i][columnIndexes[j]];
@@ -21,49 +23,71 @@ const organizeCratesByColumns = (crateSection, columnIndexes) => {
   return cratesByColumns;
 };
 
+const parseMoves = (move) => {
+  // get amount of crates to move
+  let crateAmount = '';
+  const crateAmountStartIndex = move.indexOf(' ') + 1;
+  const crateAmountEndIndex = move.indexOf(' ', crateAmountStartIndex + 1);
+  const crateAmountLength = crateAmountEndIndex - crateAmountStartIndex;
+
+  if (crateAmountLength === 1) {
+    crateAmount = move[crateAmountStartIndex];
+  } else if (crateAmountLength === 2) {
+    crateAmount = move[crateAmountStartIndex] + move[crateAmountStartIndex + 1];
+  }
+
+  const indexOfWordFrom = move.indexOf('from');
+  const moveFromStartIndex = move.indexOf(' ', indexOfWordFrom) + 1;
+  const moveFromEndIndex = move.indexOf(' ', moveFromStartIndex + 1);
+  const moveFromLength = moveFromEndIndex - moveFromStartIndex;
+
+  // get which crate column to move crates from
+  let moveFrom = '';
+  if (moveFromLength === 1) {
+    moveFrom = move[moveFromStartIndex];
+  } else if (moveFromLength === 2) {
+    moveFrom = move[moveFromStartIndex] + move[moveFromStartIndex + 1];
+  }
+
+  // get which crate column to move crates to
+  let moveTo = '';
+  const indexOfWordTo = move.indexOf('to');
+  const moveToStartIndex = move.indexOf(' ', indexOfWordTo) + 1;
+  moveTo = move[moveToStartIndex];
+
+  return { crateAmount, moveFrom, moveTo };
+};
+
+const moveOneByOne = (parsedMoves, cratesByColumns) => {
+  const { moveFrom, moveTo } = parsedMoves;
+  cratesByColumns[moveTo].unshift(cratesByColumns[moveFrom].shift());
+  return cratesByColumns;
+};
+
+const moveMultiple = (parsedMoves, cratesByColumns) => {
+  const { crateAmount, moveFrom, moveTo } = parsedMoves;
+  const crateColumnSlice = cratesByColumns[moveFrom].splice(0, crateAmount);
+  cratesByColumns[moveTo].unshift(...crateColumnSlice);
+  return cratesByColumns;
+};
+
 const moveCrates = (moves, cratesByColumns) => {
+  // copy crates
+  let movedCratesOneByOne = JSON.parse(JSON.stringify(cratesByColumns));
+  let movedCratesMultiple = JSON.parse(JSON.stringify(cratesByColumns));
+  // move crates for every move
   for (let i = 0; i < moves.length; i++) {
-    const move = moves[i];
-
-    let crateAmount = '';
-    const crateAmountStartIndex = move.indexOf(' ') + 1;
-    const crateAmountEndIndex = move.indexOf(' ', crateAmountStartIndex + 1);
-    const crateAmountLength = crateAmountEndIndex - crateAmountStartIndex;
-
-    if (crateAmountLength === 1) {
-      crateAmount = move[crateAmountStartIndex];
-    } else if (crateAmountLength === 2) {
-      crateAmount =
-        move[crateAmountStartIndex] + move[crateAmountStartIndex + 1];
-    }
-
-    const indexOfWordFrom = move.indexOf('from');
-    const moveFromStartIndex = move.indexOf(' ', indexOfWordFrom) + 1;
-    const moveFromEndIndex = move.indexOf(' ', moveFromStartIndex + 1);
-    const moveFromLength = moveFromEndIndex - moveFromStartIndex;
-
-    let moveFrom = '';
-    if (moveFromLength === 1) {
-      moveFrom = move[moveFromStartIndex];
-    } else if (moveFromLength === 2) {
-      moveFrom = move[moveFromStartIndex] + move[moveFromStartIndex + 1];
-    }
-
-    const indexOfWordTo = move.indexOf('to');
-    const moveToStartIndex = move.indexOf(' ', indexOfWordTo) + 1;
-
-    let moveTo = '';
-
-    moveTo = move[moveToStartIndex];
+    const parsedMoves = parseMoves(moves[i]);
+    const { crateAmount } = parsedMoves;
 
     for (let j = 0; j < parseInt(crateAmount); j++) {
-      cratesByColumns[moveTo].unshift(cratesByColumns[moveFrom].shift());
+      // move crates one by one (part1)
+      movedCratesOneByOne = moveOneByOne(parsedMoves, movedCratesOneByOne);
     }
-    moveFrom = '';
-    moveTo = '';
-    crateAmount = '';
+    // move crates multiple at time (part2)
+    movedCratesMultiple = moveMultiple(parsedMoves, movedCratesMultiple);
   }
-  return cratesByColumns;
+  return { movedCratesOneByOne, movedCratesMultiple };
 };
 
 const getTopCrates = (cratesByColumns) => {
@@ -84,6 +108,7 @@ fs.readFile('data.txt', 'utf-8', (err, data) => {
     dataAsArray.reduce((a, b) => (a.length <= b.length ? a : b))
   );
 
+  // determine which raw string indexes are for different columns
   let columnIndexes = [];
   for (let i = 0; i < dataAsArray[indexOfSectionBreak - 1].length; i++) {
     if (parseInt(dataAsArray[indexOfSectionBreak - 1][i]) > 0) {
@@ -91,12 +116,15 @@ fs.readFile('data.txt', 'utf-8', (err, data) => {
     }
   }
 
+  // get the crates section from the data
   let crateSection = dataAsArray.slice(0, indexOfSectionBreak - 1);
   const cratesByColumns = organizeCratesByColumns(crateSection, columnIndexes);
-  const movedCrates = moveCrates(
+  const { movedCratesOneByOne, movedCratesMultiple } = moveCrates(
     dataAsArray.slice(indexOfSectionBreak + 1),
     cratesByColumns
   );
-  const topCrates = getTopCrates(movedCrates);
-  console.log('top-crates: ', topCrates);
+  const topCratesMovedOneByOne = getTopCrates(movedCratesOneByOne);
+  const topCratesMovedMultiple = getTopCrates(movedCratesMultiple);
+  console.log('top-crates-moved-one-by-one: ', topCratesMovedOneByOne);
+  console.log('top-crates-moved-multiple: ', topCratesMovedMultiple);
 });
